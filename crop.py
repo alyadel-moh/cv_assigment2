a86cb931eb788815d24155a8d45c3d8236696ba1
bassam-14 <mohamedbassam1442004.wednesday@gmail.com>
2026-04-18 07:50:01 +0200

    Finished Overlaying

--- crop.py
+++ crop.py
@@ -1,17 +1,38 @@
 import numpy as np
-def crop_ar_frame(ar_frame,book_corners):
+
+
+def remove_black_borders(img, tol=10):
+    # Create a mask of pixels that are NOT black
+    mask = img > tol
+    # If the image is color, check if any of the channels are not black
+    if img.ndim == 3:
+        mask = mask.any(axis=2)
+
+    # Find the bounding box of the non-black content
+    rows = np.any(mask, axis=1)
+    cols = np.any(mask, axis=0)
+
+    # Get the min/max coordinates
+    ymin, ymax = np.where(rows)[0][[0, -1]]
+    xmin, xmax = np.where(cols)[0][[0, -1]]
+
+    return img[ymin : ymax + 1, xmin : xmax + 1]
+
+
+def crop_ar_frame(ar_frame, book_corners):
+    clean_frame = remove_black_borders(ar_frame)
     tl, tr, br, bl = book_corners
     book_width = (np.linalg.norm(tr - tl) + np.linalg.norm(br - bl)) / 2.0
     book_height = (np.linalg.norm(bl - tl) + np.linalg.norm(br - tr)) / 2.0
     target_aspect = book_width / book_height
-    src_h , src_w = ar_frame.shape[:2]
+    src_h, src_w = clean_frame.shape[:2]
     src_aspect = src_w / src_h
     if src_aspect > target_aspect:
         new_w = int(src_h * target_aspect)
         x_start = (src_w - new_w) // 2
-        cropped = ar_frame[:, x_start:x_start+new_w]
+        cropped = clean_frame[:, x_start : x_start + new_w]
     else:
         new_h = int(src_w / target_aspect)
         y_start = (src_h - new_h) // 2
-        cropped = ar_frame[y_start:y_start+new_h, :]
+        cropped = clean_frame[y_start : y_start + new_h, :]
     return cropped
---
+++ overlay.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from homography import compute_homography
+import matplotlib.pyplot as plt
+
+
+def overlay_ar_frame(main_frame, cropped_ar_frame, target_corners):
+    ar_h, ar_w = cropped_ar_frame.shape[:2]
+    main_h, main_w = main_frame.shape[:2]
+
+    # Define 4 corners of cropped AR frame
+    src_corners = np.array(
+        [[0, 0], [ar_w - 1, 0], [ar_w - 1, ar_h - 1], [0, ar_h - 1]], dtype=np.float32
+    )
+
+    dst_corners = target_corners.astype(np.float32)
+
+    # Compute  Homography matrix & warps it
+    H_overlay = compute_homography(src_corners, dst_corners)
+    warped_ar = cv2.warpPerspective(cropped_ar_frame, H_overlay, (main_w, main_h))
+
+    # Create mask to hollow the book area
+    mask = np.zeros((main_h, main_w), dtype=np.uint8)
+    cv2.fillConvexPoly(mask, dst_corners.astype(np.int32), 255)
+
+    mask_inv = cv2.bitwise_not(mask)
+    main_bg = cv2.bitwise_and(main_frame, main_frame, mask=mask_inv)
+
+    return cv2.add(main_bg, warped_ar)
+
+
+def display_frame(frame):
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    plt.figure(figsize=(12, 8))
+    plt.imshow(rgb_frame)
+    plt.title("Frame Overlayed")
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
